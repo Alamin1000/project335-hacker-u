807c560849d6747fdcb3cfdd68b483ba2b2dd7b1
alamin <alaminhosen44000@gmail.com>
2023-05-31 23:43:20 +0600

    In

---
+++ js/main.js
@@ -0,0 +1,89 @@
+(function ($) {
+  "use strict";
+
+  // offcanvas-js
+  $(".offcanvas-open").click(function () {
+    $(".offcanvas-menu").addClass("active");
+    $(".offcanvas-overlay").addClass("active");
+  });
+  $(".offcanvas-menu a").click(function () {
+    $(".offcanvas-menu").removeClass("active");
+    $(".offcanvas-overlay").removeClass("active");
+  });
+  $(".close-offcanvas").click(function () {
+    $(".offcanvas-menu").removeClass("active");
+    $(".offcanvas-overlay").removeClass("active");
+  });
+  $(document).mouseup(function (e) {
+    var container = $(".offmenu");
+
+    // If the target of the click isn't the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $(".offcanvas-menu").removeClass("active");
+      $(".offcanvas-overlay").removeClass("active");
+    }
+  });
+
+  // owl-carousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 50,
+    responsiveClass: true,
+    nav: false,
+    navText: [
+      '<span class="fas fa-chevron-left fa-2x"></span>',
+      '<span class="fas fa-chevron-right fa-2x"></span>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  //magnificPopup
+  $(".popup-img").magnificPopup({
+    type: "image",
+  });
+  $(".popup-video").magnificPopup({
+    type: "iframe",
+  });
+  // magnific-popup-img-gallery
+  $(".album-img a").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+  //multiple-img-gallery
+  $(".image-box").each(function () {
+    // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: ".img", // the selector for gallery item
+      type: "image",
+      gallery: {
+        enabled: true,
+      },
+    });
+  });
+})(jQuery);
+
+$(document).ready(function () {
+  // scroll up
+  $(function () {
+    $.scrollUp();
+  });
+
+  // preloader
+  $("#preloader").fadeOut(500);
+});
